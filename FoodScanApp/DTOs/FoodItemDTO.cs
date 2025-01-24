45aa34a7f72713d387a3945ab8d52812b81afff2
mustafa-arezo <mustafa_arezoo@hotmail.com>
2025-01-24 10:28:57 +0100

    Add TODOs and comment out FoodItemId

--- FoodScanApp/DTOs/FoodItemDTO.cs
+++ FoodScanApp/DTOs/FoodItemDTO.cs
@@ -3,16 +3,20 @@
 namespace FoodScanApp.DTOs
 {
     /// <summary>
-    /// Vad vill jag VISA?
-    /// Lista på livsmedel (ex Bregott smör, Ingedienser + Någon Fakta om ingredienserna)
+    /// What do I want to display to the USER
+    /// List on Livsmedel (e.g Bregott + Ingedients)
     /// </summary>
     public class FoodItemDTO
     {
-        public int FoodItemId { get; set; }
-        public int LivsmedelsTypId { get; set; }
+        //public int FoodItemId { get; set; }
         public int Nummer { get; set; }
+        public int LivsmedelsTypId { get; set; }
         public string Namn { get; set; }
         public string VetenskapligtNamn { get; set; }
         public List<IngredientDTO> Ingredienser { get; set; }
     }
+
+    //TODO: Make a translation of the properties in the class in the FRONTEND!!
+    //TODO: Figure out whether to use LivsmedelsTypId or LivsmedelsTyp and what it will be used for!
+    //TODO: How can we GET and display Information and health risk? Maybe start with the logic of High Sugar content?
 }
